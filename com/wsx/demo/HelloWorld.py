85fbf5b4f28d95d0a5bd9b33d0bcf8e10cc0ed84
Wangsx <wangsixxx@sina.com>
2017-12-11 17:15:51 +0800

    Initial commit

---
+++ com/wsx/demo/HelloWorld.py
@@ -0,0 +1,7 @@
+print('hello world')
+print('the quick brown fox', 'jumps over', 'the lazy dog')
+print('100 + 200 = ', 100 + 200)
+name = input('Please enter your name:')
+print('hello', name)
+print('I\'m ok.')
+print('I\'m learning\nPython')
