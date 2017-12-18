dd3761dc9f102c3a900a31d25115bac7bd7c4fa0
Wangsx <wangsixxx@sina.com>
2017-12-18 17:02:05 +0800

    continue learn grammar

---
+++ com/wsx/demo/if.py
@@ -0,0 +1,16 @@
+number = 17
+guess = int(input('Enter an Integer : '))
+if guess == number:
+    print('Congratulations, you guessed it.')
+    # New block start here
+    print('but you do not win any prizes!')
+    # New block end here
+elif guess < number:
+    print('No, it is litter higher than that')
+    # Another block
+    # You can do whatever you want in this block...
+else:
+    print('No, it is little lower than that')
+    # You most have guess > number to reach here
+print('Done')
+# The last statement always executed, after if statement executed
--- com/wsx/demo/var.py
+++ com/wsx/demo/var.py
@@ -2,10 +2,14 @@ i = 5
 print(i)
 i = i + 1
 print(i)
+# 三引号可输出句子中的行转换和单引双引号
 s = '''this is multi-line string.
-this is the second line.'''
+this is the second line.
+"What's your name?" I asked,
+He said "Bond, James Bond."
+'''
 print(s)
-# 显示行连接 Explicit Line Joining
+# 显示行连接 Explicit Line Joining，句末\表示本行还在继续
 s = 'This is a string. \
 this is continue the string'
 print(s)
