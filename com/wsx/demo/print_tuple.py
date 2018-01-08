783c38d0d002d6172326b120d1c286c76ff119a7
Wangsx <wangsixxx@sina.com>
2018-01-08 17:25:46 +0800

    learn python grammar

--- com/wsx/demo/print_tuple.py
+++ com/wsx/demo/print_tuple.py
@@ -3,5 +3,5 @@ age = 22
 name = "Jackson"
 # 格式化输出打印，%s表示字符，%d表示数字，分别对应元组中的数据类型
 print('%s is %d years old' % (name, age))
-# 当print函数中元素只有一个时可以省略括号，称为定制
+# 当print函数中元素只有一个时可以定制，即省略括号
 print('Why is %s playing with that python?' % name)
